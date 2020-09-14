bb056437f83bb00e02667bac96b5564e875108f9
Amir Azizi <amirazizi67@gmail.com>
2020-09-14 09:59:40 +0430

    اضافه کردن ماژول پنل و لاگین

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { PanelModule } from './panel/panel.module';
+import { LoginModule } from './login/login.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,14 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		LoginModule,
+		PanelModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
